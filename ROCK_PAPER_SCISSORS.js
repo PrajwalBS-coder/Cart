a18141bcd7c92ce5ded38bd6ef784a62d752bfd1
Prajwal B S <prajwaljhon@gmail.com>
2024-09-17 13:08:05 +0530

    rock paper scissors game created

---
+++ ROCK_PAPER_SCISSORS.js
@@ -0,0 +1,49 @@
+let rock = document.getElementById("rock")
+let paper = document.getElementById('paper')
+let scissors = document.getElementById('scissors')
+let result = document.getElementById('result')
+let result2 = document.getElementById('result2')
+let system = 0
+let rocky = 0
+let papery = 0
+let scissorsy = 0
+let systemwincount = 0
+let usercount = 0
+rock.addEventListener('click', () => {
+    system = getRandomInt(3)
+    console.log(getRandomInt(3));
+
+    if (system === 2) {
+        usercount += 1
+        console.log("USER", usercount)
+
+    }
+    if (system === 0) {
+
+    }
+    if (system === 1) {
+        systemwincount += 1
+        console.log("SYSTEm", systemwincount)
+    }
+    result.textContent = usercount;
+    result2.textContent = systemwincount;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(getRandomInt(3));
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
