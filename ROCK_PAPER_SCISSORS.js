df1fb76a0fb162ddeea5b1231bf274d41bf4f976
Prajwal B S <prajwaljhon@gmail.com>
2024-09-23 23:03:18 +0530

    Tried to modal for pop up image It's not working properly its'showing previous result to current result

--- ROCK_PAPER_SCISSORS.js
+++ ROCK_PAPER_SCISSORS.js
@@ -10,8 +10,7 @@ let papery = 0
 let scissorsy = 0
 let systemwincount = 0
 let usercount = 0
-let op=``
-
+let op = ``;
 rock.addEventListener('click', () => {
     system = getRandomInt(3)
     console.log(getRandomInt(3));
@@ -21,7 +20,7 @@ rock.addEventListener('click', () => {
         console.log("USER", usercount)
         winner = new Audio("ku.mp3")
         winner.play()
-        op += `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        op += `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -29,7 +28,7 @@ rock.addEventListener('click', () => {
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        ...
+                         <img src="good tumbsup.webp" alt="">
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -37,10 +36,8 @@ rock.addEventListener('click', () => {
                     </div>
                 </div>
             </div>
-        </div>`
-        pop.innerHTML = op;
-
-
+        </div>`;
+       
 
     }
     if (system === 0) {
@@ -51,12 +48,35 @@ rock.addEventListener('click', () => {
     if (system === 1) {
         systemwincount += 1
         console.log("SYSTEm", systemwincount)
-        systemwin = new Audio("lost this round.mp3")
-        systemwin.play()
+        system = new Audio("lost this round.mp3")
+        system.play()
+        op += `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                         <img src="good tumbsdown.webp" alt="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+        // pop.innerHTML = op;
+        // op = ``
     }
     result.textContent = usercount;
     result2.textContent = systemwincount;
+     pop.innerHTML = op;
+        op = ``
+
 })
+
 
 
 
