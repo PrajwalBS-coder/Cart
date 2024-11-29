d72ee402798d2d4988daa890ef4fa7ae9bb0f7fb
Prajwal B S <prajwaljhon@gmail.com>
2024-11-29 11:03:09 +0530

    Added Code For MoveRight,MoveLeft arrow keys and it's working Fine✌😎

--- DOODLE JUMP.js
+++ DOODLE JUMP.js
@@ -2,14 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50;
-    let doodlerBottomSpace = 150;
+    let startpoint=150
+    let doodlerBottomSpace = startpoint;
     let IsGameOver = false
     let PlatformCount = 5
     let Platforms = []
     let UpTimerId
     let LowTimerId
-    let IsJumping = false
-
+    let IsJumping = true
+    let IsGoingleft=false
+    let IsGoingRight=false
+    let LeftTimerId
+    let RightTimerId
 
 
 
@@ -71,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
         UpTimerId = setInterval(() => {
             doodlerBottomSpace += 20
             doodler.style.bottom = doodlerBottomSpace + 'px'
-            if (doodlerBottomSpace > 350) {
+            if (doodlerBottomSpace > startpoint +200) {
 
                 Fall()
             }
@@ -101,6 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     !IsJumping
                 ) {
                     console.log('Landed On Bar')
+                    startpoint=doodlerBottomSpace
                     Jump()//34min
 
                 }
@@ -128,9 +133,11 @@ document.addEventListener('DOMContentLoaded', () => {
     function Control(e) {
         if (e.key == "ArrowLeft") {
             //left move function
+            MoveLeft()
         }
         else if (e.key == "ArrowRight") {
             //right move function
+            MoveRight()//44.41
 
         }
         else if (e.key == "ArrowUp") {
@@ -139,6 +146,46 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    //move Left Function
+
+    function MoveLeft(){
+        if(IsGoingRight){
+            clearInterval(RightTimerId)
+            IsGoingRight=false
+
+        }
+         IsGoingleft=true
+         LeftTimerId=setInterval(()=>{
+            if (doodlerLeftSpace>=0)
+            {
+            doodlerLeftSpace-=5
+            doodler.style.left=doodlerLeftSpace + 'px'
+            }
+            else{
+                MoveRight()
+            }
+
+         },30)
+
+    }
+    function MoveRight(){
+        if(IsGoingleft){
+            clearInterval(LeftTimerId)
+            IsGoingleft=false
+
+        }
+        IsGoingRight=true
+        RightTimerId=setInterval(()=>{
+            if(doodlerLeftSpace<=340)
+            {
+                doodlerLeftSpace+=5
+                 doodler.style.left=doodlerLeftSpace + 'px'
+
+            }
+
+        },30)
+
+    }
 
 
     //Strating Doodler
@@ -149,6 +196,7 @@ document.addEventListener('DOMContentLoaded', () => {
             CreateDoodler()
             setInterval(MovePlatForm, 30)
             Jump()
+            document.addEventListener('keyup',Control)
         }
 
     }
